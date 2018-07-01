58fc97b9a566e9fbe43ac6ae2fa346305e1d0f2b
Imran Chowdhury <imranc.408@gmail.com>
2018-07-01 22:19:38 +1000

    Added campground + user association

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,12 +16,16 @@ router.get('/', (req, res) => {
 });
 
 // CREATE - add new campground to db
-router.post('/', (req, res) => {
+router.post('/', isLoggedIn, (req, res) => {
   // get data from form
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc};
+  var author = {
+      id: req.user._id,
+      username: req.user.username
+  };
+  var newCampground = {name: name, image: image, description: desc, author: author};
   // create new campground and save to DB (using model)
   Campground.create(newCampground, (err, newlyCreated) => {
       if (err) {
@@ -29,13 +33,14 @@ router.post('/', (req, res) => {
       }
       else {
           // redirect back to campgrounds page
+          console.log(newlyCreated);
           res.redirect('/campgrounds');
       }
   });
 });
 
 // NEW - show form to create new campground
-router.get('/new', (req, res) => res.render('campgrounds/new'));
+router.get('/new', isLoggedIn, (req, res) => res.render('campgrounds/new'));
 
 // SHOW - shows more information about one campground
 router.get('/:id', (req, res) => {
@@ -50,5 +55,13 @@ router.get('/:id', (req, res) => {
         }
     });
 });
+
+// middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 module.exports = router;
