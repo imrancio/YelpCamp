6f9c3dfe52d03b60c1948557f50896eaae49bc16
Imran Chowdhury <imranc.408@gmail.com>
2018-07-01 21:51:53 +1000

    user object reference for comment author

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-// set up seed database
-// seedDB();
+// seedDB(); // seed the database
 
 // PASSPORT CONFIG
 app.use(require('express-session')({
--- routes/comments.js
+++ routes/comments.js
@@ -34,9 +34,15 @@ router.post('/', isLoggedIn, (req, res) => {
                     console.log(err);
                 }
                 else {
+                    // add username and id to comment
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    // save comment
+                    comment.save();
                     // associate new comment to campground
                     campground.comments.push(comment);
                     campground.save();
+                    console.log(comment);
                     // redirect to campground show page
                     res.redirect('/campgrounds/' + campground._id);
                 }
