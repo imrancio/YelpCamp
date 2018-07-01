c43f2328d71344862642a09a8239a567644d1ecb
Imran Chowdhury <imranc.408@gmail.com>
2018-07-01 11:43:52 +1000

    Added nested comments routes, updated show, refactored views

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express     = require('express'),
     bodyParser  = require('body-parser'),
     mongoose    = require('mongoose'),
     Campground  = require('./models/campground'),
+    Comment     = require('./models/comment'),
     seedDB      = require('./seeds');
 
 // Connect or create MongoDB through mongoose
@@ -10,7 +11,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 // set up seed database
-seedDB();
+// seedDB();
 
 // RESTful Routes
 
@@ -26,7 +27,7 @@ app.get('/campgrounds', (req, res) => {
          console.log(err);
      }
      else {
-         res.render('index', {campgrounds: allCampgrounds});
+         res.render('campgrounds/index', {campgrounds: allCampgrounds});
      }
   });
 });
@@ -51,9 +52,7 @@ app.post('/campgrounds', (req, res) => {
 });
 
 // NEW - show form to create new campground
-app.get('/campgrounds/new', (req, res) => {
-  res.render('new');
-});
+app.get('/campgrounds/new', (req, res) => res.render('campgrounds/new'));
 
 // SHOW - shows more information about one campground
 app.get('/campgrounds/:id', (req, res) => {
@@ -65,9 +64,52 @@ app.get('/campgrounds/:id', (req, res) => {
         else {
             console.log(foundCampground);
             // render show template with that campground
-            res.render('show', {campground: foundCampground});
+            res.render('campgrounds/show', {campground: foundCampground});
         }
     });
 });
+
+// NESTED COMMENTS ROUTES
+
+// NEW COMMENT Form
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+    // find campground by ID
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('comments/new', {campground: campground});
+        }
+    });
+
+});
+
+// CREATE COMMENT
+app.post('/campgrounds/:id/comments', (req, res) => {
+    // lookup campground using id
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/campgrounds');
+        }
+        else {
+            // create new comment
+            Comment.create(req.body.comment, (err, comment) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    // associate new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    // redirect to campground show page
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+        }
+    });
+});
+
 
 app.listen(3000, console.log('YelpCamp server started on port 3000'));
